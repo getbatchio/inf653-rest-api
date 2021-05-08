43a4c6b9b3e40d237048eee9a8f482224b026f26
getbatchio <75768830+getbatchio@users.noreply.github.com>
2021-05-08 02:18:22 -0400

    update reset

--- index.php
+++ index.php
@@ -38,7 +38,7 @@
                     ?>
                 </select>
                 <input type = "submit" class="btn btn-primary" />
-                <input id="resetQuotesListForm" type="reset" class="button red button-slim">
+                <input type = "submit" id="resetQuotesListForm" type="reset" class="btn btn-ptimary">
             </form>
             <table class="table table-hover">
                 <tr>
